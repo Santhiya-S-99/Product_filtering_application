429d99e0c36f4d5bccf5ff4cfe2e155884caa3b6
santhiya <santhiya.s@heptagon.in>
2023-05-12 17:24:12 +0530

    product_filtering_application

---
+++ src/app/product.model.ts
@@ -0,0 +1,11 @@
+export interface Product {
+    id: Number;
+    product_name: any;
+    product_id : any;
+    description: String;
+    category_name : String;
+    category_id : any;
+    in_stock : String;
+    quantity: Number;
+    price : Number;  
+}
