5f0cd7b44b2383b23e36b3baa7cd9fd7c6edd25d
santhiya <santhiya.s@heptagon.in>
2023-05-12 17:56:14 +0530

    changes on filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ export class AppComponent {
       product_id: '10HO27', 
       description: "A curved or angled stick used in playing hockey", 
       category_name: 'Sports', 
-      category_id: 'accessories5', 
+      category_id: 'sports12', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 2143 
@@ -25,7 +25,7 @@ export class AppComponent {
       product_id: '10WS01', 
       description: "Loudspeakers that receive audio signals using radio frequency (RF) waves rather than over audio cables", 
       category_name: 'Electronics', 
-      category_id: 'clothing1', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 24242 
@@ -35,7 +35,7 @@ export class AppComponent {
       product_id: '10AN09', 
       description: "Angular is a data binding framework using an MVC (Model View Controller) pattern", 
       category_name: 'Books', 
-      category_id: 'accessories5', 
+      category_id: 'books15', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 970 
@@ -45,7 +45,7 @@ export class AppComponent {
       product_id: '10CS11', 
       description: "Manufactures and markets the Crocs brand of foam clogs", 
       category_name: 'Clothing & Shoes', 
-      category_id: 'accessories5', 
+      category_id: 'clothing18', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 567
@@ -55,7 +55,7 @@ export class AppComponent {
       product_id: '10BA15', 
       description: "They cover objects such as shower baskets, soap dispensers, toilet roll holders and towel rails", 
       category_name: 'Beauty and personal Care Products', 
-      category_id: 'accessories5', 
+      category_id: 'beauty17', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 214 
@@ -65,7 +65,7 @@ export class AppComponent {
       product_id: '10VE20', 
       description: "A container (such as a cask, bottle, kettle, cup, or bowl) for holding something", 
       category_name: 'House and Kitchen Supplies', 
-      category_id: 'accessories5', 
+      category_id: 'house16', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 356 
@@ -75,7 +75,7 @@ export class AppComponent {
       product_id: '10TE24', 
       description: "It has an oval frame with a taut interlaced network of strings", 
       category_name: 'Sports', 
-      category_id: 'accessories5', 
+      category_id: 'sports12', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 443 
@@ -85,7 +85,7 @@ export class AppComponent {
       product_id: '10PL08', 
       description: "Electric cord that has two or three metal pins that connect the cord to a source of electricity", 
       category_name: 'Electronics', 
-      category_id: 'kitchen2', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 890 
@@ -95,7 +95,7 @@ export class AppComponent {
       product_id: '10FO16', 
       description: "To prevent infection, odors and an injury to tissue", 
       category_name: 'Beauty and personal Care Products', 
-      category_id: 'accessories5', 
+      category_id: 'beauty17', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 533 
@@ -105,7 +105,7 @@ export class AppComponent {
       product_id: '10RE10', 
       description: "React is the only library that has been extensively used and documented to create large, complex web applications", 
       category_name: 'Books', 
-      category_id: 'accessories5', 
+      category_id: 'books15', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 250 
@@ -115,7 +115,7 @@ export class AppComponent {
       product_id: '10MO02', 
       description: "Electronic visual computer display that includes a screen, circuitry and the case in which that circuitry is enclosed", 
       category_name: 'Electronics', 
-      category_id: 'beauty4', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 432 
@@ -125,7 +125,7 @@ export class AppComponent {
       product_id: '10MA23', 
       description: "Spices used in Indian cuisine, most often containing cardamom, coriander, mace together with pepper, nutmeg, fennel seeds, jeera", 
       category_name: 'House and Kitchen Supplies', 
-      category_id: 'accessories5', 
+      category_id: 'house16', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 567 
@@ -135,7 +135,7 @@ export class AppComponent {
       product_id: '10SH12', 
       description: "Pants that end above the knee or reach the knee", 
       category_name: 'Clothing & Shoes', 
-      category_id: 'accessories5', 
+      category_id: 'clothing18', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 6547 
@@ -146,7 +146,7 @@ export class AppComponent {
       product_id: '10SO14', 
       description: "Piece of clothing worn on the feet and often covering the ankle or some part of the calf", 
       category_name: 'Clothing & Shoes', 
-      category_id: 'accessories5', 
+      category_id: 'clothing18', 
       in_stock: 'Yes',
       quantity: 1, 
       price: 535 
@@ -156,7 +156,7 @@ export class AppComponent {
       product_id: '10VO21', 
       description: "A spherical or ovoid body used in a game or sports ball", 
       category_name: 'Sports', 
-      category_id: 'accessories5', 
+      category_id: 'sports12', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 657 
@@ -166,7 +166,7 @@ export class AppComponent {
       product_id: '10HA17', 
       description: "A cream that you put on your hands to make them feel softer and smoother", 
       category_name: 'Beauty and personal Care Products', 
-      category_id: 'accessories5', 
+      category_id: 'beauty17', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 456 
@@ -176,7 +176,7 @@ export class AppComponent {
       product_id: '10RE07', 
       description: "Scientific book which is systematically written, based on facts and the results of a research, and analysed critically, through rational arguments", 
       category_name: 'Books', 
-      category_id: 'accessories5', 
+      category_id: 'books15', 
       in_stock: 'Yes',
       quantity: 1, 
       price: 50 
@@ -186,7 +186,7 @@ export class AppComponent {
       product_id: '10CE22', 
       description: "Made from clay that has been shaped and then baked ... See more at ceramic.", 
       category_name: 'House and Kitchen Supplies', 
-      category_id: 'accessories5', 
+      category_id: 'house16', 
       in_stock: 'Yes',
       quantity: 1, 
       price: 875 
@@ -196,7 +196,7 @@ export class AppComponent {
       product_id: '10TS13', 
       description: "Style of fabric shirt named after the T shape of its body and sleeves", 
       category_name: 'Clothing & Shoes', 
-      category_id: 'accessories5', 
+      category_id: 'clothing18', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 345 
@@ -207,7 +207,7 @@ export class AppComponent {
       product_id: '10MO04', 
       description: "Small, movable device that lets you control a range of things on a computer", 
       category_name: 'Electronics', 
-      category_id: 'accessories5', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 5467 
@@ -217,7 +217,7 @@ export class AppComponent {
       product_id: '10HA19', 
       description: "Electric heating coils and a fan that blows the air", 
       category_name: 'Beauty and personal Care Products', 
-      category_id: 'accessories5', 
+      category_id: 'beauty17', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 324 
@@ -227,7 +227,7 @@ export class AppComponent {
       product_id: '10SC06', 
       description: "One that leads or directs another on a course", 
       category_name: 'Books', 
-      category_id: 'accessories5', 
+      category_id: 'books15', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 567 
@@ -237,7 +237,7 @@ export class AppComponent {
       product_id: '10TE05', 
       description: "mass medium for advertising, entertainment, news, and sports", 
       category_name: 'Electronics', 
-      category_id: 'accessories5', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 5978 
@@ -247,7 +247,7 @@ export class AppComponent {
       product_id: '10OI25', 
       description: "Liquid fat used in frying, baking, and other types of cooking.", 
       category_name: 'House and Kitchen Supplies', 
-      category_id: 'accessories5', 
+      category_id: 'house16', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 512 
@@ -257,7 +257,7 @@ export class AppComponent {
       product_id: '10SC18', 
       description: "Perfume scented the air", 
       category_name: 'Beauty and personal Care Products', 
-      category_id: 'accessories5', 
+      category_id: 'beauty17', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 35 
@@ -268,7 +268,7 @@ export class AppComponent {
       product_id: '10PE26', 
       description: "protection, entertainment, or as an act of compassion", 
       category_name: 'Pet Accessories', 
-      category_id: 'accessories5', 
+      category_id: 'pet14', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 54 
@@ -279,7 +279,7 @@ export class AppComponent {
       product_id: '10RO28', 
       description: "The activity of moving along the ground using roller skates", 
       category_name: 'Sports', 
-      category_id: 'accessories5', 
+      category_id: 'sports12', 
       in_stock: 'Yes',
       quantity: 1, 
       price: 150 
@@ -289,7 +289,7 @@ export class AppComponent {
       product_id: '10LA03', 
       description: "A laptop can be easily transported and used in temporary spaces such as on airplanes", 
       category_name: 'Electronics', 
-      category_id: 'accessories5', 
+      category_id: 'electronics13', 
       in_stock: 'Yes', 
       quantity: 1,
       price: 850 
